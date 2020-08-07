b6f7a429d903631623334f44e2d675cd5c7f3f02
TM85dev <t.maczka85@wp.pl>
2020-08-07 09:59:08 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 const cards = [];
 const flippedCards = [];
-let score = {total: 0, tries: 0, pairs: 0}
+let score = {total: 0, tries: 0, pairs: 0};
 let toggle = false;
 
 for(let i=0; i<=5; i++) {
@@ -11,49 +11,36 @@ for(let i=0; i<=5; i++) {
 }
 
 cards.sort(function(a, b) {
-	return 0.5 - Math.random()
-})
+	return 0.5 - Math.random();
+});
 
 
 /* CLICK CARD */
 cards.map((card, index) => {
 	$("img").eq(index).click(function() {
-		if($(this).attr("id") != "pair") {
 
 			/* FIRST CARD */
 			if(toggle === false) {
-				$(this).transition({
-					scaleX: 0
-				},200);
 				setTimeout(() => {
 					$(this).attr("src", card.src);	// CHANGING FIRST CARD TO ANIMAL
 					flippedCards[0] = card.src;
 					$(this).attr("id", "flipped1");
 					$(this).attr("id", "flipped");
-					$(this).transition({
-						scaleX: 1
-					},200)
-				},200)
-				
-				toggle = true	// SWITCHING TO SECOND CARD
+				},200);
+				$(this).css("pointer-events", "none");
+				toggle = true;	// SWITCHING TO SECOND CARD
 			}
 	
 			/* SECOND CARD */
 			else {
 				toggle = false;
 				if($(this).attr("src") === "./assets//card_back.png") {
-					$(this).transition({
-						scaleX: 0
-					},200)
 					setTimeout(() => {
 						$(this).attr("src", card.src);	// CHANGING SECOND CARD TO ANIMAL
 						flippedCards[1] = card.src;
 						$(this).attr("id", "flipped2");
 						$(this).attr("id", "flipped");
-						$(this).transition({
-							scaleX: 1
-						},200);
-					},200)
+					},200);
 					$("img").css("pointer-events", "none");
 					setTimeout(() => {
 						if(flippedCards[0] === flippedCards[1]) {	// FLIP CARDS WHEN SUCCESS
@@ -68,29 +55,23 @@ cards.map((card, index) => {
 							setTimeout( () => {
 								$(".earn-point").transition({
 									opacity: 0
-								})
+								});
 								setTimeout(() => {
-									$(".earn-point").css({bottom: 0})
-								},600)
-							}, 1000)
+									$(".earn-point").css({bottom: 0});
+								},600);
+							}, 1000);
 							score.total += 1;
 							score.tries += 1;
 							score.pairs += 1;
 						}
 						else {
-							$("img#flipped").transition({	// FLIP CARDS WHEN MISS
-								scaleX: 0
-							},200)
 							$("img").attr("src", "./assets//card_back.png");	//FLIP BACK ALL CARDS
 							setTimeout(() => {
-								$("img#flipped").transition({
-									scaleX: 1
-								},200)
 								$("img#flipped").attr("id", "");					
 							},200);
 							score.total -= 1;
 							if(score.total < 0) {
-								score.total = 0
+								score.total = 0;
 							}
 							score.tries += 1;
 						}
@@ -104,16 +85,15 @@ cards.map((card, index) => {
 
 						/* END GAME */
 						if(score.pairs === 6) {
-							$("<div class='overlay'><input type='button' value='restart game'></div>").appendTo("body")
+							$("<div class='overlay'><input type='button' value='restart game'></div>").appendTo("body");
 						}
 						$(".overlay input").click(() => {
 							location.reload();
-						})
-					},1000)
+						});
+					},1000);
 				}
 			}
 
-		}
 	})
 })
 
